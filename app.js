e8c648a74782c550c4a3669fa16c8731401c0b3c
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-09-13 16:12:31 +0800

    feat: add welcome page and add login function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const port = 3000
 const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const User = require("./models/user")
 const app = express()
 
 mongoose.connect("mongodb://localhost/account-list", {
@@ -21,8 +23,50 @@ db.once("open", () => {
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
 app.set("view engine", "hbs")
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
   res.render("index")
+})
+
+app.post("/", (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  let errorType = 0
+  let verifiedAccount = {}
+  let checkStatus = ""
+  User.find()
+    .lean()
+    .then(users => {
+      for (const user of users) {
+        if (user.email === email && user.password === password) {
+          checkStatus = "Correct"
+          verifiedAccount = user
+          return { checkStatus, verifiedAccount }
+        } else {
+          checkStatus = "Failed"
+          verifiedAccount = {}
+          if (user.email === email && user.password !== password) {
+            errorType = 1
+            return { errorType }
+          }
+        }
+      }
+      return { checkStatus, verifiedAccount }
+    })
+    .then(user => {
+      if (checkStatus === "Correct") {
+        res.render("welcome", { firstName: verifiedAccount.firstName })
+      } else if (checkStatus === "Failed") {
+        if (errorType === 1) {
+          errorMessage = "密碼錯誤"
+        } else {
+          errorMessage = "查無使用者資料"
+        }
+        res.render("index", { errorMessage })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
